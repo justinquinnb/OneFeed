b997cbb18f14a95a783671108ee0076960f82bd6
Justin Quinn <justinquinnb@gmail.com>
2025-03-18 00:34:53 -0400

    Make FormattingIndexers TextFormatting-type-agnostic

--- src/main/java/com/justinquinnb/onefeed/customization/textstyle/TextFormattingIndexer.java
+++ src/main/java/com/justinquinnb/onefeed/customization/textstyle/TextFormattingIndexer.java
@@ -1,23 +1,23 @@
 package com.justinquinnb.onefeed.customization.textstyle;
 
-// TODO consider removing the generic as implementations can (seemingly) successfully be TextFormatting-type-agnostic
 /**
  * A type capable of translating marked-up text into an index of {@link TextFormatting}s mapped to where they occur
- * @param <T> the language of {@link TextFormatting}s that may be derived from marked-up text
  */
-public interface TextFormattingIndexer<T extends TextFormatting> {
+public interface TextFormattingIndexer {
     /**
      * Builds a {@link FormattingKit} for the provided {@code markedUpText} by applying the processes specified by
      * each {@link FormattingRule} in the {@link FormattingRuleset} to each substring matching its regex.
      *
+     * @param <T> the language of {@link TextFormatting}s that may be derived from marked-up text
      * @param markedUpText plaintext marked up using some language that the {@code formattingRules} may recognize
      * @param formattingRules the {@code FormattingRuleset} specifying what to do for each type of marked-up substring
      *                        in {@code markedUpText} as it's encountered
+     *
      * @return a {@code FormattingKit} pairing a markup-less {@code markedUpText} instance with an index of
      * substring locations mapped to the {@code TextFormatting}(s) that should be applied to them. Should the provided
      * {@code formattingRules} not match any parts of the {@code markedUpText} (as might be when its markup
      * language isn't recognized by the rules), the {@code FormattingKit} will contain {@code markedUpText} as-is with
      * an empty index of formattings.
      */
-    public FormattingKit<T> buildKitFor(String markedUpText, FormattingRuleset<T> formattingRules);
+    public <T extends TextFormatting> FormattingKit<T> buildKitFor(String markedUpText, FormattingRuleset<T> formattingRules);
 }
