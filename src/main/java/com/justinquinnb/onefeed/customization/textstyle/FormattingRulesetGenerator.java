ecd57665ef43beb33db447cb40626dba887b938f
Justin Quinn <justinquinnb@gmail.com>
2025-05-04 21:21:56 -0400

    Complete FormattingRulesetGenerator
    -Finish FormattingRulesetGenerator
    -Add logging to generator and TextFormattingRegistry

--- src/main/java/com/justinquinnb/onefeed/customization/textstyle/FormattingRulesetGenerator.java
+++ src/main/java/com/justinquinnb/onefeed/customization/textstyle/FormattingRulesetGenerator.java
@@ -6,12 +6,18 @@ import com.justinquinnb.onefeed.customization.textstyle.application.QuickFormatA
 import com.justinquinnb.onefeed.customization.textstyle.markup.MarkupLanguage;
 import com.justinquinnb.onefeed.customization.textstyle.markup.TextFormatting;
 import com.justinquinnb.onefeed.customization.textstyle.markup.TextFormattingRegistry;
+import com.justinquinnb.onefeed.customization.textstyle.parsing.FormatParsingRule;
 import com.justinquinnb.onefeed.customization.textstyle.parsing.FormatParsingRuleset;
+import com.justinquinnb.onefeed.customization.textstyle.parsing.QuickFormatParsingRuleset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+// TODO add logging
 /**
  * A type capable of generating {@link FormatApplicationRuleset}s and {@link FormatParsingRuleset}s that represent a
  * defined {@link MarkupLanguage}.
@@ -24,6 +30,8 @@ public class FormattingRulesetGenerator {
     This seems like the best approach
      */
 
+    private static final Logger logger = LoggerFactory.getLogger(FormattingRulesetGenerator.class);
+
     /**
      * A mapping of each {@link MarkupLanguage} type to its respective {@link FormatApplicationRuleset} so that ruleset
      * retrieval does not entail building (what should be) an unchanging ruleset each time.
@@ -32,11 +40,10 @@ public class FormattingRulesetGenerator {
             new HashMap<>();
 
     /**
-     * Maps unique {@link FormatParsingRuleset} names to their respective {@code FormatParsingRuleset}s. Names are used
-     * since hash equality is impossible given the need to test lambda equality for the underlying FormatParsingRules,
-     * which isn't possible.
+     * A mapping of each {@link MarkupLanguage} type to its respective {@link FormatApplicationRuleset} so that ruleset
+     * retrieval does not entail building (what should be) an unchanging ruleset each time.
      */
-    private static final HashMap<String, FormatParsingRuleset> CACHED_PARSING_RULESETS =
+    private static final HashMap<Class<? extends MarkupLanguage>, FormatParsingRuleset> CACHED_PARSING_RULESETS =
             new HashMap<>();
 
     /**
@@ -53,34 +60,40 @@ public class FormattingRulesetGenerator {
     public static FormatApplicationRuleset getApplicationRuleset(Class<? extends MarkupLanguage> markupLang)
             throws UnknownTextStyleEntityException
     {
+        logger.debug("Getting the application ruleset for {}", markupLang.getSimpleName());
+
         // If the markup language's application ruleset does not exist in the cache already, add it
         if (!CACHED_APPLICATION_RULESETS.containsKey(markupLang)) {
+            logger.trace("An application ruleset for {} has not been cached yet", markupLang.getSimpleName());
             cacheApplicationRulesetFor(markupLang);
         }
 
         return CACHED_APPLICATION_RULESETS.get(markupLang);
     }
 
-    // TODO finish implementing
     /**
+     * Gets last-cached {@link FormatParsingRuleset} for the desired {@code markupLang}. If no ruleset exists in
+     * the cache, a new one is generated and cached before being returned.
      *
-     * @param markupLang
-     * @return
+     * @param markupLang the {@link MarkupLanguage} type whose {@code FormatApplicationRuleset} to get
+     *
+     * @return a {@code FormatParsingRuleset} specifying how the desired {@code markupLang}'s recognized
+     * {@link TextFormatting}s should appear as markup in text
+     * @throws UnknownTextStyleEntityException if the provided {@code markupLang} does not exist in the
+     * {@link TextFormattingRegistry}
      */
-    public static FormatParsingRuleset getParsingRulesetFor(Class<? extends MarkupLanguage> markupLang) {
+    public static FormatParsingRuleset getParsingRulesetFor(Class<? extends MarkupLanguage> markupLang)
+            throws UnknownTextStyleEntityException
+    {
+        logger.debug("Getting the parsing ruleset for {}", markupLang.getSimpleName());
+
         // If the markup language's parsing ruleset does not exist in the cache already, add it
-        String rulesetKey = getParsingRulesetNameFor(markupLang);
-        if (!CACHED_PARSING_RULESETS.containsKey(rulesetKey)) {
+        if (!CACHED_PARSING_RULESETS.containsKey(markupLang)) {
+            logger.trace("A parsing ruleset for {} has not been cached yet", markupLang.getSimpleName());
             cacheParsingRulesetFor(markupLang);
         }
 
-        return CACHED_PARSING_RULESETS.get(rulesetKey);
-    }
-
-    // TODO finish implementing
-    public static FormatParsingRuleset getParsingRulesetFor(
-            Class<? extends MarkupLanguage> markupLang, LinkedHashSet<Class<? extends TextFormatting>> parsingOrder) {
-
+        return CACHED_PARSING_RULESETS.get(markupLang);
     }
 
     /**
@@ -95,53 +108,164 @@ public class FormattingRulesetGenerator {
     private static void cacheApplicationRulesetFor(Class<? extends MarkupLanguage> markupLang)
             throws UnknownTextStyleEntityException
     {
+        logger.debug("Caching the application ruleset for {}", markupLang.getSimpleName());
+
         // Determine the TextFormatting types recognized by the markupLang
         Class<? extends TextFormatting>[] formattingsInLang = TextFormattingRegistry
                 .getFormattingsSupportedBy(markupLang);
 
-        // Get the set of parsing rules from the TextFormattingRegistry for the language
+        // For easier debugging, make this list a nice string of the class names
+        String fmtsInLangStr = "[";
+        for (Class<? extends TextFormatting> formatting : formattingsInLang) {
+            fmtsInLangStr += formatting.getSimpleName();
+            if (formatting != formattingsInLang[formattingsInLang.length - 1]) {
+                fmtsInLangStr += ", ";
+            }
+        }
+        fmtsInLangStr += "]";
+
+        logger.trace("Determined {}'s supported formattings: {}", markupLang.getSimpleName(), fmtsInLangStr);
+
+        // Get the set of application rules from the TextFormattingRegistry for the language
         Set<FormatApplicationRule> applicationRules = new LinkedHashSet<>();
         for (Class<? extends TextFormatting> formatting : formattingsInLang) {
             applicationRules.add(TextFormattingRegistry.getApplicationRuleFor(formatting, markupLang));
+            logger.trace("Added application rule for {}", formatting.getSimpleName());
         }
 
         // Build a FormattingApplicationRuleset from these rules
-        CACHED_APPLICATION_RULESETS.put(markupLang, new QuickFormatApplicationRuleset("Auto-Generated " +
-                markupLang.getSimpleName(), applicationRules));
+        if (CACHED_APPLICATION_RULESETS.put(markupLang, new QuickFormatApplicationRuleset("Auto-Generated " +
+                markupLang.getName(), applicationRules)) == null)
+        {
+            logger.debug("Cached new application ruleset for {}", markupLang.getSimpleName());
+        } else {
+            logger.debug("Updated existing application ruleset in cache for {}", markupLang.getSimpleName());
+        }
     }
 
-    // TODO make this iterate through the existing hashmap and regenerate each ruleset
-    // this method should be invoked when new customizations are loaded at runtime(?). depends on if I add that
-    // capability
     /**
-     * test
-     */
-    public static void refreshCachedApplicationRulesets() {
-
-    }
-
-    // HELPERS
-    // TODO document
-    /**
+     * Creates and caches the {@link FormatParsingRuleset} for the desired {@code markupLang}. If a ruleset for the
+     * desired {@code markupLang} was already cached, this method will update it in place.
      *
-     * @param markupLang
-     * @return
-     */
-    private static String getParsingRulesetNameFor(Class<? extends MarkupLanguage> markupLang) {
-        return "AutoOrdered" + markupLang.getSimpleName();
-    }
-
-    // TODO document
-    /**
+     * @param markupLang the type of {@link MarkupLanguage} to make and cache a {@code FormatParsingRuleset} for
      *
-     * @param markupLang
-     * @param parsingOrder
-     * @return
+     * @throws UnknownTextStyleEntityException if the provided {@code markupLang} is unknown to the
+     * {@link TextFormattingRegistry}, where the ruleset is derived from
      */
-    private static String getParsingRulesetNameFor(
-            Class<? extends MarkupLanguage> markupLang,
-            LinkedHashSet<Class<? extends TextFormatting>> parsingOrder)
+    private static void cacheParsingRulesetFor(Class<? extends MarkupLanguage> markupLang)
+            throws UnknownTextStyleEntityException
     {
-        return "CustomOrder" + parsingOrder.hashCode() + markupLang.getSimpleName();
+        logger.debug("Caching the parsing ruleset for {}", markupLang.getSimpleName());
+
+        // Determine the TextFormatting types recognized by the markupLang
+        Class<? extends TextFormatting>[] formattingsInLang = TextFormattingRegistry
+                .getFormattingsSupportedBy(markupLang);
+
+        // For easier debugging, make this list a nice string of the class names
+        String fmtsInLangStr = "[";
+        for (Class<? extends TextFormatting> formatting : formattingsInLang) {
+            fmtsInLangStr += formatting.getSimpleName();
+            if (formatting != formattingsInLang[formattingsInLang.length - 1]) {
+                fmtsInLangStr += ", ";
+            }
+        }
+        fmtsInLangStr += "]";
+
+        logger.trace("Determined {}'s supported formattings: {}", markupLang.getSimpleName(), fmtsInLangStr);
+
+        // Get the set of parsing rules from the TextFormattingRegistry for the language
+        LinkedHashSet<FormatParsingRule> parsingRules = new LinkedHashSet<>();
+        for (Class<? extends TextFormatting> formatting : formattingsInLang) {
+            parsingRules.add(TextFormattingRegistry.getParsingRuleFor(formatting, markupLang));
+            logger.trace("Added parsing rule for {}", formatting.getSimpleName());
+        }
+
+        // Build a FormattingParsingRuleset from these rules
+        if (CACHED_PARSING_RULESETS.put(markupLang, new QuickFormatParsingRuleset("Auto-Generated " +
+                markupLang.getName(), parsingRules)) == null)
+        {
+            logger.debug("Cached new parsing ruleset for {}", markupLang.getSimpleName());
+        } else {
+            logger.debug("Updated existing parsing ruleset in cache for {}", markupLang.getSimpleName());
+        }
+    }
+
+    /**
+     * Creates and caches the {@link FormatApplicationRuleset} and {@link FormatParsingRuleset} for the desired
+     * {@code markupLang}. If a ruleset for the desired {@code markupLang} was already cached, this method will update
+     * it in place.
+     *
+     * @param markupLang the type of {@link MarkupLanguage} to make and cache a {@link FormatApplicationRuleset} and
+     * {@link FormatParsingRuleset} for
+     *
+     * @throws UnknownTextStyleEntityException if the provided {@code markupLang} is unknown to the
+     * {@link TextFormattingRegistry}, where the ruleset is derived from
+     */
+    public static void cacheRulesetsFor(Class<? extends MarkupLanguage> markupLang)
+            throws UnknownTextStyleEntityException
+    {
+        logger.debug("Caching the rulesets for {}", markupLang.getSimpleName());
+        cacheApplicationRulesetFor(markupLang);
+        cacheParsingRulesetFor(markupLang);
+    }
+
+    /**
+     * Clears both ruleset caches entirely and repopulates them with the most recent rules provided by the
+     * {@link TextFormattingRegistry}.
+     */
+    public static void refreshCache() {
+        logger.debug("Refreshing the ruleset cache");
+        logger.trace("Clearing both caches");
+        CACHED_APPLICATION_RULESETS.clear();
+        CACHED_PARSING_RULESETS.clear();
+
+        // Repopulate the cache language by language
+        Class<? extends MarkupLanguage>[] knownLangs = TextFormattingRegistry.getKnownLanguages();
+        for (Class<? extends MarkupLanguage> markupLang : knownLangs) {
+            try {
+                logger.trace("Generating the ruleset for {}", markupLang.getSimpleName());
+                cacheRulesetsFor(markupLang);
+            } catch (UnknownTextStyleEntityException e) {
+                // Continue with the others
+            }
+        }
+    }
+
+    /**
+     * Clears only rulesets that are no longer known by the {@link TextFormatting} and adds/updates the cached rulesets
+     * for all others.
+     */
+    public static void smartCacheRefresh() {
+        logger.debug("Smart refreshing the ruleset cache");
+
+        // Remove any forgotten languages
+        // Since the cache's ruleset languages match each other (since they are both from the TextFormattingRegistry),
+        // only one keyset is necessary
+        Set<Class<? extends MarkupLanguage>> cachedApplicationLangs = CACHED_APPLICATION_RULESETS.keySet();
+        Set<Class<? extends MarkupLanguage>> knownLangs = Set.of(TextFormattingRegistry.getKnownLanguages());
+
+        for (Class<? extends MarkupLanguage> markupLang : cachedApplicationLangs) {
+            if (!knownLangs.contains(markupLang)) {
+                logger.trace("MarkupLanguage {} no longer exists in the TextFormattingRegistry, so removing it from " +
+                        "the cache", markupLang.getSimpleName());
+                CACHED_APPLICATION_RULESETS.remove(markupLang);
+                CACHED_PARSING_RULESETS.remove(markupLang);
+            } else {
+                logger.trace("MarkupLanguage {} still exists in the TextFormattingRegistry, so keeping it",
+                        markupLang.getSimpleName());
+            }
+        }
+
+        // Add and update any remaining languages
+        for (Class<? extends MarkupLanguage> markupLang : knownLangs) {
+            try {
+                logger.trace("Regenerating the ruleset for {}", markupLang.getSimpleName());
+                cacheRulesetsFor(markupLang);
+            } catch (UnknownTextStyleEntityException e) {
+                logger.warn("An UknownTextStyleEntityException occurred while generating the ruleset for {}",
+                        markupLang.getSimpleName());
+                // Proceed as nothing can be done. This should never happen.
+            }
+        }
     }
 }
