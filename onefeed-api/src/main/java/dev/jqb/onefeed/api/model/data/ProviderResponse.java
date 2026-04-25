1db209d94408b4d9ccbfe0b434fdd83f5451c680
Justin Quinn <67286549+justinquinnb@users.noreply.github.com>
2026-04-25 17:36:53 -0400

    Add support for webhook-based content retrieval

--- onefeed-api/src/main/java/dev/jqb/onefeed/api/model/data/ProviderResponse.java
+++ onefeed-api/src/main/java/dev/jqb/onefeed/api/model/data/ProviderResponse.java
@@ -15,22 +15,10 @@ import lombok.Setter;
  * @param <Out> the type of {@link RawContent} contained in this response
  */
 @Getter @Setter
-public class ProviderResponse<Out extends RawContent> {
-
+public class ProviderResponse<Out extends RawContent> extends ContentPackage<Out> {
     /**
-     * The requested content, normalized and ready for distribution
-     */
-    @NonNull
-    private List<Out> content;
-
-    /**
-     * The {@link ContentFilter}s that were used to derive the {@link #content}
+     * The {@link ContentFilter}s that were used to derive the content
      */
     @NonNull
     private List<ContentFilter<?>> appliedFilters;
-
-    /**
-     * The time at which {@code this} respones was generated
-     */
-    private Instant generated;
 }
