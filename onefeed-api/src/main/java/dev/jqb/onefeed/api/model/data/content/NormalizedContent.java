d961e861e913550b3cd97d4d75461b1f380e31b1
Justin Quinn <67286549+justinquinnb@users.noreply.github.com>
2026-04-30 17:25:53 -0400

    Make Content an abstract class

--- onefeed-api/src/main/java/dev/jqb/onefeed/api/model/data/content/NormalizedContent.java
+++ onefeed-api/src/main/java/dev/jqb/onefeed/api/model/data/content/NormalizedContent.java
@@ -7,24 +7,6 @@ import java.time.Instant;
  * {@link Provider}-generated content that has since been normalized,
  * making it ready for caching and distribution by OneFeed
  */
-public abstract class NormalizedContent implements ContentInfo {
-    /**
-     * Info identifying the origin of the content piece
-     */
-    private SourceInfo source;
-
-    /**
-     * The time at which the content was published
-     */
-    private Instant published;
-
-    @Override
-    public SourceInfo getSource() {
-        return source;
-    }
-
-    @Override
-    public Instant getPublished() {
-        return published;
-    }
+public abstract class NormalizedContent extends Content {
+    
 }
