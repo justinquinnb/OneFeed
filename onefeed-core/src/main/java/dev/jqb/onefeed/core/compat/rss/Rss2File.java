9ef318648f5d0d5fd26030377677f5b89ed76c2f
Justin Quinn <justinquinnb@gmail.com>
2026-07-11 22:16:29 -0400

    Create adapter and Rss2File types
    - Create an adapter class for Feeds to RssChannels so that all Feeds can function as an RssChannel, even without being an implementation itself, via the default mandated normalizer
    - Add a Rss2File type to wrap channels in outputted RSS XML documents

---
+++ onefeed-core/src/main/java/dev/jqb/onefeed/core/compat/rss/Rss2File.java
@@ -0,0 +1,35 @@
+package dev.jqb.onefeed.core.compat.rss;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import tools.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+/**
+ * The root element of an RSS file
+ *
+ * @see <a href="https://www.rssboard.org/rss-specification#whatIsRss">RSS 2.0 Specification</a>
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonRootName("rss")
+public class Rss2File {
+    @JacksonXmlProperty(isAttribute = true, localName = "version")
+    private String version = "2.0";
+
+    /**
+     * The RSS channel the file contains
+     */
+    private RssChannel channel;
+
+    /**
+     * Constructs a new {@code Rss2File} with the given channel.
+     * @param channel the channel of the RSS file
+     */
+    public Rss2File(RssChannel channel) {
+        this.version = version;
+        this.channel = channel;
+    }
+}
